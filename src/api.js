126aa0b107d622d1e97ff832b8e448d3e8b4c3f7
Sumerian812 <veronica_hannibal@outlook.com>
2021-04-11 15:13:52 +0200

    api setup

---
+++ src/api.js
@@ -0,0 +1,16 @@
+const base_url = "https://api.rawg.io/api";
+const apiKey = process.env.REACT_APP_API_KEY;
+
+const date = () => {
+    const day = String(new Date().getDate()).padStart(2, '0');
+    const month = String(new Date().getMonth()).padStart(2, '0')
+    const year = new Date().getFullYear();
+    return {
+        todayLastYear: `${year - 1}-${month}-${day}`,
+        today: `${year}-${month}-${day}`,
+        todayNextYear: `${year + 1}-${month}-${day}`
+    };
+}
+
+export const popularGamesUrl = `${base_url}/games?key=${apiKey}?dates=${date().todayLastYear},
+${date().today},&ordering=-rating&page_size=10`;
