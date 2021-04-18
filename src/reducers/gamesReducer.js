5f902dde08ae917d3e11239131779e81524b388a
Sumerian812 <veronica_hannibal@outlook.com>
2021-04-18 12:40:12 +0200

    redux setup

---
+++ src/reducers/gamesReducer.js
@@ -0,0 +1,18 @@
+
+const initState = {
+    popularGames: [],
+    newGames: [],
+    upcomingGames: [],
+    searched: []
+};
+
+const gamesReducer = (state = initState, action) => {
+    switch (action.type) {
+        case "FETCH_GAMES":
+            return { ...state };
+        default:
+            return { ...state };
+    }
+};
+
+export default gamesReducer;
