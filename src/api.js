080d2a6687e1a9a4576dac5d60ea92f7b6e01ac7
Sumerian812 <veronica_hannibal@outlook.com>
2021-05-23 13:38:08 +0200

    fetch more pop. games

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ const base_url = "https://api.rawg.io/api";
 const apiKey = process.env.REACT_APP_API_KEY;
 
 // get games
-export const popularGamesUrl = `${base_url}/games/lists/main?key=${apiKey}&page_size=20&ordering=-relevance`;
+export const popularGamesUrl = `${base_url}/games/lists/main?key=${apiKey}&page_size=28&ordering=-relevance`;
 export const newGamesThisWeekUrl = `${base_url}/games/lists/recent-games?key=${apiKey}&ordering=-added&page_size=10`;
 export const newGamesNextWeekUrl = `${base_url}/games/lists/recent-games-future?key=${apiKey}&ordering=-added&page_size=10`;
 
